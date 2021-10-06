ab4f7e385a40a5dcc0bc159bdba9ef986c0af60d
Simeon2001 <jesusanyasimeon@gmail.com>
2021-10-06 11:38:54 +0100

    'new'

--- shortner/settings.py
+++ shortner/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600,sslmode=True)
+db_from_env = dj_database_url.config(conn_max_age=600,ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
