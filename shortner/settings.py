ba6745a1444e3c6a58efbb7f912412f47a72e54a
Simeon2001 <jesusanyasimeon@gmail.com>
2021-10-07 14:38:40 +0100

    'new'

--- shortner/settings.py
+++ shortner/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import pymongo
-from pymongo import MongoClient
+import dj_database_url
+#import pymongo
+#from pymongo import MongoClient
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +85,7 @@ DATABASES = {
     }
 }
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
@@ -95,9 +96,9 @@ DATABASES = {
 
     }
 }
-
-#db_from_env = dj_database_url.config(conn_max_age=600)
-#DATABASES['default'].update(db_from_env)
+"""
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
